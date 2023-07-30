ce1176e1433bee2fef3bf9738bb15b963cb34cac
jesus <jdrch62@gmail.com>
2023-07-30 17:01:12 -0400

    Avances con el frontend

--- src/client/src/MainContainer.jsx
+++ src/client/src/MainContainer.jsx
@@ -4,8 +4,22 @@ import "./MainContainer.css"
 function MainContainer () {
     return <div className="main-cat-container">
         <div className="main-cat-container-img">
-            <div></div>
-            <div></div>
+            <div className="main-cat-container-img-1">
+                <div className="inside-cat-container-img-1">
+                    <div>
+                        <div className="logo-inverse-container">Cat wiki
+                            <img src="../images/invertido.png" alt="" />
+                        </div>
+                        <div><p>Get to know more about your cat breed</p></div>
+                    </div>
+                </div>
+
+                <div>
+
+                </div>
+
+            </div>
+            <div className="main-cat-container-img-2"></div>
         </div>
         <div></div>
     </div>
