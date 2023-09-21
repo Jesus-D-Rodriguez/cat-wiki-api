9710ad49f3784b1214facbb33936f302e703b82b
jesus <jdrch62@gmail.com>
2023-09-21 16:56:46 -0400

    Cambio en link

--- src/server/index.js
+++ src/server/index.js
@@ -7,7 +7,7 @@ const axios = require("axios");
 const Cat = require("../server/models/Cat.js")
 
 const corsOptions = {
-  origin: 'https://cat-wiki-zlzc.onrender.com/', // Cambia esto con el origen de tu aplicación
+  origin: 'https://cat-wiki-zlzc.onrender.com', // Cambia esto con el origen de tu aplicación
 };
 
 app.use(cors(corsOptions));
