197257f0b657e0797aba284221dcbfbdc14604e6
jesus <jdrch62@gmail.com>
2023-09-20 21:25:05 -0400

    Cambio en link de db

--- src/server/database.js
+++ src/server/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
 
-  const uri = "mongodb+srv://cat-wiki-user:cat-wiki-user@cat-wiki-db.axsvspt.mongodb.net/?retryWrites=true&w=majority";
+  const uri = "mongodb+srv://cat-wiki-user:cat-wiki-user@cat-wiki-db.axsvspt.mongodb.net/cat-wiki-db-db?retryWrites=true&w=majority";
   const uri2 = 'mongodb://127.0.0.1:27017/cat-wiki';
   try {
     await mongoose.connect(uri, {
