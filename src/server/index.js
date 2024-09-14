258b5eca4d30b075acf513a15390b32c3f631e7d
jesus <jdrch62@gmail.com>
2024-09-13 21:34:02 -0400

    correccion de error

--- src/server/index.js
+++ src/server/index.js
@@ -6,8 +6,12 @@ const connectDB = require("./database.js");
 const axios = require("axios");
 const Cat = require("../server/models/Cat.js")
 
+const url = 'https://cat-wiki-zlzc.onrender.com';
+const local = 'http://localhost:5173';
+
 const corsOptions = {
-  origin: 'https://cat-wiki-zlzc.onrender.com', // Cambia esto con el origen de tu aplicación
+  origin: local, 
+
 };
 
 app.use(cors(corsOptions));
@@ -40,7 +44,7 @@ connectDB().then(() => {
             let image;
 
             let response_images;
-            console.log("Referencia image id:", breed.reference_image_id);
+            //console.log("Referencia image id:", breed.reference_image_id);
             const API_KEY = "live_GO6QtjM1mtzVMLBqc6kwZmzWvCZZnIl6xrdM9xR0mtOWcDz4pkWZMa7HZwDuJ8Xa";
             const n_images = 8;
             const images_url_1 = `https://api.thecatapi.com/v1/images/search?limit=${n_images}&breed_ids=${breed.id}&api_key=${API_KEY}`;
@@ -49,43 +53,50 @@ connectDB().then(() => {
             const response2 = await axios.get(`https://api.thecatapi.com/v1/images/${breed.reference_image_id}`);
             const response3 = await axios.get(images_url_1);
             response_images = response3.data;
-            console.log("Response 3 " + response3.data.length + " "+ images_url_1);
+            //console.log("Response 3 " + response3.data.length + " "+ images_url_1);
             image = response2.data;
             
-            console.log("image url: ",image.url);
+            //console.log("image url: ",image.url);
 
             if (image) {
-              const newCat = new Cat({
-                id: breed.id,
-                name: breed.name,
-                cfa_url: breed.cfa_url,
-                description: breed.description,
-                temperament: breed.temperament,
-                origin: breed.origin,
-                life_span: breed.life_span,
-                adaptability: breed.adaptability,
-                affection_level: breed.affection_level,
-                child_friendly: breed.child_friendly,
-                grooming: breed.grooming,
-                intelligence: breed.intelligence,
-                health_issues: breed.health_issues,
-                social_needs: breed.social_needs,
-                stranger_friendly: breed.stranger_friendly,
-                reference_image_id: breed.reference_image_id,
-                image_url: image.url,
-                images_1:(response_images.length > 0)? response_images[0].url : null,
-                images_2:(response_images.length > 1)? response_images[1].url : null,
-                images_3:(response_images.length > 2)? response_images[2].url : null,
-                images_4:(response_images.length > 3) ? response_images[3].url : null,
-               images_5:(response_images.length > 4) ? response_images[4].url : null,
-                images_6:(response_images.length > 5) ? response_images[5].url : null,
-                images_7:(response_images.length > 6) ? response_images[6].url : null,
-                images_8:(response_images.length > 7)  ? response_images[7].url : null
-              });
 
-              await newCat.save();
+              const existingCat = await Cat.findOne({ id: breed.id });
+              if (!existingCat) {
+                //console.log("GUARDADOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO");
+                const newCat = new Cat({
+                  id: breed.id,
+                  name: breed.name,
+                  cfa_url: breed.cfa_url,
+                  description: breed.description,
+                  temperament: breed.temperament,
+                  origin: breed.origin,
+                  life_span: breed.life_span,
+                  adaptability: breed.adaptability,
+                  affection_level: breed.affection_level,
+                  child_friendly: breed.child_friendly,
+                  grooming: breed.grooming,
+                  intelligence: breed.intelligence,
+                  health_issues: breed.health_issues,
+                  social_needs: breed.social_needs,
+                  stranger_friendly: breed.stranger_friendly,
+                  reference_image_id: breed.reference_image_id,
+                  image_url: image.url,
+                  images_1:(response_images.length > 0)? response_images[0].url : null,
+                  images_2:(response_images.length > 1)? response_images[1].url : null,
+                  images_3:(response_images.length > 2)? response_images[2].url : null,
+                  images_4:(response_images.length > 3) ? response_images[3].url : null,
+                 images_5:(response_images.length > 4) ? response_images[4].url : null,
+                  images_6:(response_images.length > 5) ? response_images[5].url : null,
+                  images_7:(response_images.length > 6) ? response_images[6].url : null,
+                  images_8:(response_images.length > 7)  ? response_images[7].url : null
+                });
+  
+                await newCat.save();
+  
+                break; 
+              }
 
-              break; // 
+
             } else {
               xd = xd + 1;
               console.log("No hubo imagen ni gato: ", breed.name)
